e1c72c19bf97dc2a21b1bcf9756b8cbfc23be1b0
Xu Tan(Lucian) <tan.xu1@northeastern.edu>
2025-04-25 16:52:28 -0700

    refactor home page and migrate and update the css styles

--- client/battleship/src/pages/Home.jsx
+++ client/battleship/src/pages/Home.jsx
@@ -1,3 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaGamepad, FaBook, FaTrophy } from "react-icons/fa";
+import myImage from "../images/NPIXTXSIUNHV3ACRPMPSP6TKGM.png.avif";
+import "./home.css";
+
 export default function Home() {
-    return <h2>Welcome to Battleship</h2>;
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <h2 className="home-title">🚢 Battleship Game</h2>
+        <p className="home-subtitle">Let's Play!!!</p>
+        <div className="button-container">
+          <Link to="/games">
+            <button className="btn btn-game">
+              <FaGamepad /> All Games
+            </button>
+          </Link>
+          <Link to="/game/new">
+            <button className="btn btn-create">
+              <FaGamepad /> Create New Game
+            </button>
+          </Link>
+          <Link to="/rules">
+            <button className="btn btn-rules">
+              <FaBook /> Rules
+            </button>
+          </Link>
+          <Link to="/high-scores">
+            <button className="btn btn-scores">
+              <FaTrophy /> High Scores
+            </button>
+          </Link>
+        </div>
+        <div className="image-wrapper">
+          <img
+            src={myImage}
+            alt="Battleship Illustration"
+            className="home-image"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
