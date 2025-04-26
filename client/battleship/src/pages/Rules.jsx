5c12d9727d82f8dca6eeaf345f55955484c7210b
Xu Tan(Lucian) <tan.xu1@northeastern.edu>
2025-04-25 17:27:51 -0700

    fix typo on Rules.css import

--- client/battleship/src/pages/Rules.jsx
+++ client/battleship/src/pages/Rules.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./rules.css";
+import "./Rules.css";
 
 export default function Rules() {
   const navigate = useNavigate();
